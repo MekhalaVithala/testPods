6ec75e1d33c12576e768ac7116c871a713a9945d
Mekhala Vithala <mekhala.vithala@bottlerocketstudios.com>
2017-03-08 10:23:25 -0600

    add internal class

---
+++ MainApp/MainApp/doNothing.h
@@ -0,0 +1,15 @@
+//
+//  doNothing.h
+//  MainApp
+//
+//  Created by Mekhala Vithala on 3/8/17.
+//  Copyright © 2017 BottleRocket. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface doNothing : NSObject
+
+- (void)doNothing;
+
+@end
---
+++ MainApp/MainApp/doNothing.m
@@ -0,0 +1,17 @@
+//
+//  doNothing.m
+//  MainApp
+//
+//  Created by Mekhala Vithala on 3/8/17.
+//  Copyright © 2017 BottleRocket. All rights reserved.
+//
+
+#import "doNothing.h"
+
+@implementation doNothing
+
+-(void)doNothing {
+    
+}
+
+@end
